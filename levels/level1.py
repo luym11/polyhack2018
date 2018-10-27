1900edc82b1915c9fd7d9a04a3d4816595a4719c
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-10-27 21:59:40 +0200

    Improved safety
    Stop and disconnect drone in finally block, regardless of exceptions
    Add missing imports

--- levels/level1.py
+++ levels/level1.py
@@ -1,13 +1,17 @@
 import sys
 sys.path.append("../src")
 
-import followpath
+from followpath import followWaypoints
 import signal
 from drone import Drone
 import globals
 import api
 from path_planning import generateWaypoints
+import numpy
+import time
 
+askUser = True
+api.debugMode = True
 keepDelivering = True
 
 def terminate(sig, frame):
@@ -19,16 +23,24 @@ signal.signal(signal.SIGINT, terminate)
 droneNum = int(input("Enter drone number: ")) - 24
 droneID = globals.DRONES[droneNum]
 drone = Drone(droneID, globals.DRONEADDRS[droneID])
-while keepDelivering:
-	package = api.package(globals.SWARMNAME)
-	drone.land(0)
-	drone.getPackage(package)
-	drone.takeoff(1)
-	waypoints = generateWaypoints(drone.pos, numpy.array([package["coordinates"][0], package["coordinates"][1], package["coordinates"][2]]))
-	followWaypoints(drone, waypoints)
-	drone.land(0)
-	drone.deliver()
-	drone.takeoff(1)
-	waypoints = generateWaypoints(drone.pos, drone.home)
-	followWaypoints(drone, waypoints)
-drone.disconnect()
+try:
+	while keepDelivering:
+		package = api.package(globals.SWARMNAME)
+		drone.getPackage(package)
+		drone.currentDelivery = package["id"]
+		time.sleep(drone.takeoff(0.3))
+		waypoints = generateWaypoints(drone.pos, numpy.array([package["coordinates"][0], package["coordinates"][1], 0.3])).delete(0)
+		followWaypoints(waypoints, drone)
+		time.sleep(drone.land(0))
+		drone.deliver()
+		time.sleep(drone.takeoff(0.3))
+		waypoints = generateWaypoints(drone.pos, drone.home)
+		followWaypoints(waypoints, drone)
+		time.sleep(drone.land(0))
+		if askUser and input("Continue? [y/N]: ") != "y":
+			keepDelivering = False
+except Exception as e:
+	print(e)
+finally:
+	api.stopDrone(globals.SWARMNAME, drone.droneID)
+	drone.disconnect()
