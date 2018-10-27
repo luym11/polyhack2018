48a5aa713923d8c9dc9e185a2aa7d941be01b3af
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-10-27 18:26:24 +0200

    Add takeoff and landing calls

--- levels/level1.py
+++ levels/level1.py
@@ -14,9 +14,13 @@ droneNum = input("Enter drone number: ") - 24
 drone = Drone(globals.DRONES[droneNum], globals.DRONEADDRS[droneNum])
 while keepDelivering:
 	package = api.package(globals.SWARMNAME)
+	drone.land(0)
 	drone.getPackage(package)
+	drone.takeoff(1)
 	waypoints = generateWaypoints(drone.pos, numpy.array([package["coordinates"][0], package["coordinates"][1], package["coordinates"][2]]))
 	followWaypoints(drone, waypoints)
+	drone.land(0)
 	drone.deliver()
+	drone.takeoff(1)
 	waypoints = generateWaypoints(drone.pos, drone.home)
 	followWaypoints(drone, waypoints)
