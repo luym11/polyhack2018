94f2b8419c42a634e7589894d60c05d1f1baa9fd
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-10-27 17:52:24 +0200

    Created Drone class

---
+++ src/drone.py
@@ -0,0 +1,58 @@
+import api
+import globals
+
+class Drone:
+	"""Creates a new Drone object
+
+	Args:
+		id: Drone ID
+	"""
+	def __init__(id):
+		self.droneID = id
+		status = api.droneStatus(globals.SWARMNAME, id)
+		self.pos = (status["x"], status["y"], status["z"])
+		self.home = self.pos
+		self.packages = []
+		self.currentDelivery = ""
+	
+	def update(self):
+		status = api.droneStatus(globals.SWARMNAME, self.droneID)
+		self.pos = (status["x"], status["y"], status["z"])
+
+	def goToPoint(self, point):
+		"""Commands the drone to fly to a given point
+
+		Args:
+			point: The destination point as a tuple (x, y, z)
+
+		Returns:
+			Estimated travel time before the drone reaches the point
+		"""
+		resp = api.droneGoto(globals.SWARMNAME, self.droneID, point[0], point[1], point[2], 0, 1)
+		return resp["duration"]
+	
+	def getPackage(self, package):
+		"""Picks up a new package
+
+		Args:
+			package: Package data of new package
+
+		Returns:
+			If the package was picked up successfully, the amount of weight that can
+			still be carried by the drone. Otherwise, -1.
+		"""
+		totalWeight = sum([pkg["weight"] for pkg in self.packages])
+		if totalWeight + package["weight"] > 3:
+			return -1
+		self.packages.append(package)
+		return totalWeight - package["weight"]
+	
+	def deliver(self):
+		"""Attempts to deliver the current package
+
+		Returns:
+			Whether the delivery succeeded
+		"""
+		if self.currentDelivery != "":
+			resp = api.deliver(globals.SWARMNAME, self.droneID, self.currentDelivery)
+			return resp["success"]
